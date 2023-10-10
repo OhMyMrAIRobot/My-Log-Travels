1ae91def6b0cb55c70e4300f42137cd591e3f40e
CallMeBaby <gfhieemobilelegends@gmail.com>
2023-10-11 01:01:10 +0300

    slider

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-window.onmousedown = (event) => console.log(`x = ${event.clientX}px, y = ${event.clientY}px`);
+// slider
+let offset = 0;
+const sliderLine = document.querySelector('.slider-line');
+
+document.querySelector('.arrow-right').addEventListener('click', function(){
+    offset -= 389;
+    if (offset < -389) {
+        offset = 0;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
+
+document.querySelector('.arrow-left').addEventListener('click', function () {
+    offset += 389;
+    if (offset > 389) {
+        offset = 0;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
+
+
+
+
